5416f64d6d288d36a2e3a53e2916458cd312e863
asanzma8 <asanzma8@corp.vodafone.es>
2022-06-30 17:03:01 +0200

    updating layouts

--- src/main/kotlin/es/kprops/view/AntView.kt
+++ src/main/kotlin/es/kprops/view/AntView.kt
@@ -3,6 +3,7 @@ package es.kprops.view
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -17,49 +18,66 @@ import androidx.compose.ui.unit.dp
  * @date Jun 2022
  */
 
+
 @Preview
 @Composable
 fun createAntView() {
+    val gitButtonsColor =  ButtonDefaults.outlinedButtonColors(
+        backgroundColor = Color(0xFF7BB661),
+        contentColor = Color(0xFFF5F5F5),
+        disabledContentColor = Color(0xFF666699))
+    val copyButtonsColor =  ButtonDefaults.outlinedButtonColors(
+        backgroundColor = Color(0XFFe83151),
+        contentColor = Color(0xFFF5F5F5),
+        disabledContentColor = Color(0XFFe83151))
+
     MaterialTheme {
-
-        Column(Modifier.width(500.dp)) {
-            Row (Modifier.width(500.dp)
-                         .background(color = Color.Cyan)) {
-
+        Column {
+            Row(Modifier.background(color = Color.White)) {
                 Spacer(Modifier.width(20.dp))
 
-                OutlinedButton(
-                    modifier = Modifier.width(200.dp),
+                OutlinedButton( modifier = Modifier.width(200.dp),
+                    colors = gitButtonsColor,
                     onClick = {
                     })
-                    {
-                        Text("Pull All")
-                    }
+                {
+                    Text("Git Pull All")
+                }
 
                 Spacer(Modifier.width(10.dp))
 
-                OutlinedButton(
-                    modifier = Modifier.width(200.dp),
+                OutlinedButton(modifier = Modifier.width(200.dp),
+                    colors = gitButtonsColor,
                     onClick = {
                     })
-                    {
-                        Text("Log All 2D")
-                    }
-
-                Spacer(Modifier.width(20.dp))
+                {
+                    Text("Git Log")
+                }
             }
 
-            Spacer(Modifier.height(10.dp))
+            Spacer(Modifier.height(20.dp))
 
-            Row(
-                Modifier.width(300.dp)
-                        .background(color= Color.LightGray)) {
 
-                OutlinedButton( modifier = Modifier.width(200.dp),
+            Row(Modifier.background(color = Color.White)) {
+
+                Spacer(Modifier.width(20.dp))
+
+                OutlinedButton(modifier = Modifier.width(200.dp),
+                    colors = copyButtonsColor,
                     onClick = {
                     })
                 {
                     Text("Copy PPRD1")
+                }
+
+                Spacer(Modifier.width(10.dp))
+
+                OutlinedButton(modifier = Modifier.width(200.dp),
+                    colors = copyButtonsColor,
+                    onClick = {
+                    })
+                {
+                    Text("Copy SIT2")
                 }
             }
         }
