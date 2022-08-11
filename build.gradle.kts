576916c3ce69045598e880ece8a63169360e44eb
asanzma8 <asanzma8@corp.vodafone.es>
2022-08-11 08:13:50 +0200

    setup

---
+++ build.gradle.kts
@@ -0,0 +1,54 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+
+plugins {
+    kotlin("jvm") version "1.7.0"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev753"
+}
+
+group = "me.asanzma"
+version = "1.1"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    implementation(compose.desktop.currentOs) //windows_x64)
+    implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.7.26")
+    implementation("org.jetbrains.compose.ui:ui-util-desktop:1.2.0-alpha01-dev753")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "MainKt"
+    }
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "kpropsfinder"
+            packageVersion = "1.0.3"
+        }
+    }
+}
--- src/main/kotlin/es/kprops/domain/usecases/antcases/AntUseCaseImpl.kt
+++ src/main/kotlin/es/kprops/domain/usecases/antcases/AntUseCaseImpl.kt
@@ -17,46 +17,58 @@ class AntUseCaseImpl : AntUseCase {
     private val kenobiCommand: KenobiCommand = RepoFactory.getKenobiCommandRepo()
 
     override fun gitPullAll() {
-        println("gitPullAllKenobi")
+        println("AntUseCase - gitPullAllKenobi")
         val r: AntResult =  this.gitCommand.pullAll(AntAction("pull All Kenobi"))
-        println(r.result)
+        println("\"AntUseCase - gitPullAllKenobi  result=${r.result}")
     }
 
 
     override fun openKenobiCmd() {
         println("AntUseCase - openKenobiCmd")
         val r: AntResult = this.kenobiCommand.openKenobiCmd(AntAction("Open console Kenoby path"))
-        println("openKenobiCmd result=${r.result}")
+        println("AntUseCase - openKenobiCmd result=${r.result}")
 
     }
 
     override fun gitLog() {
-
+        println("AntUseCase - gitLog")
+        val r: AntResult = this.gitCommand.gitLog(AntAction("List the last commits log"))
+        println("AntUseCase - gitLog result=${r.result}")
     }
 
     override fun launchKenobi(){
         println("AntUseCase - launchKenobi ")
         val r: AntResult = this.kenobiCommand.launchKenobiServer(AntAction("Launch kenobi server in a new console"))
-        println("\"AntUseCase - launchKenobi sresult=${r.result}")
+        println("\"AntUseCase - launchKenobi result=${r.result}")
     }
 
     override fun launchKenobiTest(){
-
+        println("AntUseCase - launchKenobiTest ")
+        val r: AntResult = this.kenobiCommand.launchKenobiTest(AntAction("Launch kenobi tests in a new console"))
+        println("\"AntUseCase - launchKenobiTest result=${r.result}")
     }
 
     override fun copyEnvPPRD1(){
-
+        println("AntUseCase - copyEnvPPRD1 ")
+        val r: AntResult = this.kenobiCommand.copyEnvPPRD1(AntAction("Copy files to setup pprd1 env"))
+        println("\"AntUseCase - copyEnvPPRD1 result=${r.result}")
     }
 
     override fun copyEnvSit2(){
-
+        println("AntUseCase - copyEnvSit2 ")
+        val r: AntResult = this.kenobiCommand.copyEnvSit2(AntAction("Copy files to setup sit2 env"))
+        println("\"AntUseCase - copyEnvSit2 result=${r.result}")
     }
 
     override fun launchKenobiInstall(){
-
+        println("AntUseCase - launchKenobiInstall ")
+        val r: AntResult = this.kenobiCommand.launchKenobiInstall(AntAction("Launchs the install kenobi command"))
+        println("\"AntUseCase - launchKenobiInstall result=${r.result}")
     }
 
     override fun buildProKenobi(){
-
+        println("AntUseCase - buildProKenobi ")
+        val r: AntResult = this.kenobiCommand.buildProKenobi(AntAction("Make a prod compilation"))
+        println("\"AntUseCase - buildProKenobi result=${r.result}")
     }
 }
